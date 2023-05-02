0065289cc7e9dbdcc568fbdac007f777fdcc6b5a
Elias Ortega <eliasortega2011@gmail.com>
2023-05-02 15:06:24 -0400

    scrit.js funcion validar campos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
 
+var checkboxUno = document.querySelector('#checkUno');
+var inputEmail = document.querySelector('#txtCorreo');
+var botonOk = document.querySelector('#boton');
+
+function validarCampos(checkboxUno, inputEmail) {
+
+    botonOk.addEventListener('click', function () {
+
+        var mensaje = (checkboxUno.checked && inputEmail.value != '')? '¡Felicitaciones! Pronto escucharás de nosotros':'Parece que nos faltó algo...';
+
+        alert(mensaje);       
+
+    });
+
+}
+
+validarCampos(checkboxUno, inputEmail);
